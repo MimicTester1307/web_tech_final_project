57035fc5d60e2d567ff591c21b40e06d405d7ef3
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-11-29 16:05:45 +0000

    update footer content

--- templates/_footer.php
+++ templates/_footer.php
@@ -1,5 +1,19 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+</script>
+
+<footer class="text-center text-lg-start bg-dark text-muted sticky-bottom">
+        <div class="container">
+                <div>
+                        <img src="/assets/Logo+-+Star+Lab+Wind+River+Knockout.png" alt="Star Lab Logo" width="240" height="53.91">
+
+                </div>
+
+                <p style="display: inline-block;">
+                        ©️ 2021 Star Lab Corporation, a Wind River Company. <br>
+                        Created wth ❤️ by Excel Chukwu.
+                </p>
+        </div>
+</footer>
 </body>
+
 </html>
