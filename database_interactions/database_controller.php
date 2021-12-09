16edcee70415e67d1b3d319e9ca61e5215589084
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-12-09 23:17:30 +0000

    add function for adding contact details to database

--- database_interactions/database_controller.php
+++ database_interactions/database_controller.php
@@ -27,3 +27,41 @@ function fetchProducts()
         return False;
     }
 }
+
+
+/**
+ * controller function for fetching events from the database and returning 
+ * it as an associative array
+ */
+function fetchEvents()
+{
+    $crud = new CRUD;
+    $request = $crud->fetchEvents();
+
+    if ($request) {
+        $record = $crud->fetchResults();
+        if (!empty($record)) {
+            return $record;
+        } else {
+            return [];
+        }
+    } else {
+        return False;
+    }
+}
+
+/**
+ * controller function for updating contact table from contact form
+ */
+
+function updateContactTable($firstName, $lastName, $email, $industry, $country, $message, $file)
+{
+    $crud = new CRUD;
+    $request = $crud->updateContactTable($firstName, $lastName, $email, $industry, $country, $message, $file);
+
+    if ($request) {
+        return true;
+    } else {
+        return true;
+    }
+}
