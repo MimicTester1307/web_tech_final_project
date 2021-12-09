7f3abde4743bca999cf2205cd4b6b18257128933
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-12-09 17:00:24 +0000

    create dummy database connection test file

---
+++ tests/DatabaseConnectionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class DatabaseConnectionTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function phpunit_works(): void
+    {
+        self::assertTrue(true);
+    }
+}
