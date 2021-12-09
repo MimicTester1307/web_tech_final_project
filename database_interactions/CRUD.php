3faa831d8eb59d6503854d40e2582012c6f7aced
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-12-09 16:59:51 +0000

    add logic for CRUD functions

--- database_interactions/CRUD.php
+++ database_interactions/CRUD.php
@@ -19,18 +19,34 @@ class CRUD extends Database
     }
 
 
+    /**
+     * fetches the available events from the database
+     * @return mysqli_query_object
+     */
     public function fetchEvents()
     {
-        echo "pass";
+        $query = "SELECT * FROM `Events` ORDER BY Events.event_date";
+        return $this->runQuery($query);
     }
 
+
+    /**
+     * fetches system information from the database
+     * @return mysqli_query_object
+     */
     public function fetchSystems()
     {
-        echo "pass";
+        $query = "SELECT * FROM `Systems`";
+        return $this->runQuery($query);
     }
 
+    /**
+     * updates the system status and time of last_access
+     * @return mysqli_query_object
+     */
     public function updateSystem($id, $status)
     {
-        echo "pass";
+        $query = "UPDATE `Systems` SET system_status = '$status', date_of_last_check=NOW() WHERE system_id = '$id'";
+        return $this->runQuery($query);
     }
 }
