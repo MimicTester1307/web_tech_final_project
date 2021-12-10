d398d708b1dc30b4a3fdf363ebc6d7be2dee8270
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-12-10 17:30:48 +0000

    move offcanvas to header file

--- admin_access/admin_index.php
+++ admin_access/admin_index.php
@@ -2,64 +2,15 @@
 include "../templates/_admin_header.php";
 ?>
 
-<!-- OffCanvas -->
-<div class="offcanvas offcanvas-start bg-dark text-white sidebar-nav" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
 
-    <!-- Offcanvas body -->
-    <div class="offcanvas-body p-0">
-        <nav class="navbar-dark">
-            <ul class="navbar-nav">
-                <!-- Section 1: Core -->
-                <li>
-                    <div class="text-muted small fw-bold text-uppercase px-3 py-3">
-                        CORE
-                    </div>
-                </li>
 
-                <li>
-                    <a href="#" class="nav-link px-3 active">
-                        <span class="me-2">
-                            <i class="bi bi-speedometer2"></i>
-                        </span>
-                        <span>Dashboard</span>
-                    </a>
-                </li>
-
-                <!-- Section 1 Divider -->
-
-                <li class="my-4">
-                    <hr class="dropdown-divider">
-                </li>
-
-                <!-- Section 2: Systems -->
-                <li>
-                    <div class="text-muted small fw-bold text-uppercase px-3 py-3">
-                        Systems
-                    </div>
-                </li>
-
-                <li>
-                    <a href="#" class="nav-link px-3 active">
-                        <span class="me-2">
-                            <i class="bi bi-activity"></i>
-                        </span>
-                        <span>Systems</span>
-                    </a>
-                </li>
-            </ul>
-
-        </nav>
+<main class="mt-5 pt-5 container">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 fw-bold fs-3"> Dashboard</div>
+        </div>
     </div>
-</div>
-</div>
-
-
-
-
-
-
-
-
+</main>
 
 
 
