aa6fb39a3b8a2053a61be4d97a73286d648c1c81
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-12-10 23:43:10 +0000

    update scripts

--- templates/_admin_header.php
+++ templates/_admin_header.php
@@ -1,5 +1,5 @@
 <?php
-$title = $title ?? "Admin Dashboard";
+$title = $title ?? "Admin Page";
 ?>
 <!doctype html>
 <html lang="en">
@@ -13,7 +13,6 @@ $title = $title ?? "Admin Dashboard";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <link href="../styling/admin_dashboard.css" rel="stylesheet">
-    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 
