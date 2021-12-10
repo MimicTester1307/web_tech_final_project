77b81f9cacb3d5c319e5454f1e99733ba9a8bda3
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-12-10 23:41:48 +0000

    implement correct AJAX functionality

--- js/event-registration-pop.js
+++ js/event-registration-pop.js
@@ -1,15 +1,24 @@
-function submitForm() {
+var modal = document.getElementById('event-registration-modal');
+var recipient = '';
+
+modal.addEventListener('show.bs.modal', function (event) {
+  var button = event.relatedTarget;
+
+  recipient = button.getAttribute('data-bs-whatever');
+});
+
+function submitForm(e) {
   // Retrieve Form details
-  let firstName = $(document.getElementById('event-registration-fName')).val();
-  let lastName = $(document.getElementById('event-registration-lName')).val();
-  let email = $(document.getElementById('event-registration-email')).val();
-  let formData = {
+  var firstName = $(document.getElementById('event-registration-fName')).val();
+  var lastName = $(document.getElementById('event-registration-lName')).val();
+  var email = $(document.getElementById('event-registration-email')).val();
+  var formData = {
     eventFirstName: firstName,
     eventLastName: lastName,
     eventEmail: email,
+    eventId: recipient,
   };
 
-  //   alert(firstName);
   $.ajax({
     url: '../validation/event_registration_validation.php', // server url
     type: 'POST',
