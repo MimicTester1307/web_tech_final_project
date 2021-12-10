a9722ed79e82be84817a286b8c7fe227fa12fa28
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-12-10 23:43:28 +0000

    update logic; remove isset checker

--- validation/event_registration_validation.php
+++ validation/event_registration_validation.php
@@ -2,42 +2,41 @@
 require "../database_interactions/database_controller.php";
 
 $eventFormErrors = [];
+$eventId = $_POST["eventId"];
 
-if (isset($_POST["eventSubmit"])) {
-    // validation for first name
-    if (isset($_POST["eventFirstName"])) {
-        $firstName = $_POST["eventFirstName"];
+// validation for first name
+if (isset($_POST["eventFirstName"])) {
+    $firstName = $_POST["eventFirstName"];
 
-        if (empty($firstName) || !preg_match("/^[a-zA-Z-']*$/", $firstName)) {
-            $eventFormErrors["event-first-name"] = "First name is required or your entered first name invalid. <br />";
-            echo "<strong>" . $eventFormErrors["event-first-name"] . "</strong>";
-        } else {
-            $firstName = htmlspecialchars($firstName);
-        }
+    if (empty($firstName) || !preg_match("/^[a-zA-Z-']*$/", $firstName)) {
+        $eventFormErrors["event-first-name"] = "First name is required or your entered first name invalid. <br />";
+        echo "<strong>" . $eventFormErrors["event-first-name"] . "</strong>";
+    } else {
+        $firstName = htmlspecialchars($firstName);
     }
+}
 
-    // Validation for last name
-    if (isset($_POST["eventLastName"])) {
-        $lastName = $_POST["eventLastName"];
+// Validation for last name
+if (isset($_POST["eventLastName"])) {
+    $lastName = $_POST["eventLastName"];
 
-        if (empty($lastName) || !preg_match("/^[a-zA-Z-']*$/", $lastName)) {
-            $eventFormErrors["event-last-name"] = "Last name is required or your entered last name invalid. <br />";
-            echo "<strong>" . $eventFormErrors["event-first-name"] . "</strong>";
-        } else {
-            $lastName = htmlspecialchars($lastName);
-        }
+    if (empty($lastName) || !preg_match("/^[a-zA-Z-']*$/", $lastName)) {
+        $eventFormErrors["event-last-name"] = "Last name is required or your entered last name invalid. <br />";
+        echo "<strong>" . $eventFormErrors["event-first-name"] . "</strong>";
+    } else {
+        $lastName = htmlspecialchars($lastName);
     }
+}
 
-    // Validation for email
-    if (isset($_POST["eventEmail"])) {
-        $email = $_POST["eventEmail"];
+// Validation for email
+if (isset($_POST["eventEmail"])) {
+    $email = $_POST["eventEmail"];
 
-        if (empty($email) || !filter_var($_POST["eventEmail"], FILTER_VALIDATE_EMAIL)) {
-            $eventFormErrors["event-email"] = "Email is required or your entered email invalid. <br />";
-            echo "<strong>" . $eventFormErrors["event-email"] . "</strong>";
-        } else {
-            $email = htmlspecialchars($email);
-        }
+    if (empty($email) || !filter_var($_POST["eventEmail"], FILTER_VALIDATE_EMAIL)) {
+        $eventFormErrors["event-email"] = "Email is required or your entered email invalid. <br />";
+        echo "<strong>" . $eventFormErrors["event-email"] . "</strong>";
+    } else {
+        $email = htmlspecialchars($email);
     }
 }
 
@@ -45,7 +44,7 @@ if (isset($_POST["eventSubmit"])) {
 
 // Check if the event error table is empty, them update database
 if (empty($eventFormErrors)) {
-    $isTableUpdated = updateEventRegistrationTable($firstName, $lastName, $email);
+    $isTableUpdated = updateEventRegistrationTable($eventId, $firstName, $lastName, $email);
     if ($isTableUpdated) {
         echo "Event Registration Successful!";
         return true;
