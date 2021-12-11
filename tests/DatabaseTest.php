4370aa11d55250b83aeca72daa287eeabac7e76f
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-12-11 19:03:04 +0000

    update testing

---
+++ tests/DatabaseTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Panther\PantherTestCaseTrait;
+
+include ""
+
+
+final class DatabaseTest extends TestCase
+{
+    use PantherTestCaseTrait;
+    /**
+     * @test
+     */
+
+    private $database;
+    private $result;
+
+    public function testDatabaseConnection()
+    {
+        $connection = new \App\Model\DatabaseConnection;
+
+        $connection->openConnection();
+
+        $this->assertTrue(true);
+    }
+}
