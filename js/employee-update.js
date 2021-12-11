1e3c28aebefa829bef4a1e4cff021e6268792949
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-12-11 00:04:12 +0000

    add ajax handler for employee deletion

---
+++ js/employee-update.js
@@ -0,0 +1,28 @@
+// Getting the modal element
+var modal = document.getElementById('employee-choice-modal');
+var recipient = '';
+
+// Adding an event listener to listen for when modal is shown
+modal.addEventListener('show.bs.modal', function (event) {
+  var button = event.relatedTarget;
+
+  recipient = button.getAttribute('data-bs-whatever'); // retrieving the value stored in the attribute
+});
+
+// Function to delete employee using ajax
+
+function deleteEmployee(e) {
+  var formData = {
+    employeeId: recipient,
+  };
+
+  $.ajax({
+    url: '../validation/event_registration_validation.php', // server url
+    type: 'POST',
+    // async: 'false',
+    data: formData,
+    success: function (response) {
+      alert(response);
+    },
+  });
+}
